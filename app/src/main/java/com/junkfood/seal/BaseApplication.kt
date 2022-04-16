f5bb4e6315e1d0643205889b154487a9dac8144a
junkfood <junkfood@hust.edu.cn>
2022-04-16 20:51:05 +0800

    ytb-dl Android

--- app/src/main/java/com/junkfood/seal/BaseApplication.kt
+++ app/src/main/java/com/junkfood/seal/BaseApplication.kt
@@ -2,16 +2,26 @@ package com.junkfood.seal
 
 import android.app.Application
 import android.content.res.Resources
+import android.util.Log
 import com.google.android.material.color.DynamicColors
+import com.yausername.youtubedl_android.YoutubeDL
+import com.yausername.youtubedl_android.YoutubeDLException
+
 
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         res = resources
-        DynamicColors.applyToActivitiesIfAvailable(this);
+        DynamicColors.applyToActivitiesIfAvailable(this)
+        try {
+            YoutubeDL.getInstance().init(this)
+        } catch (e: YoutubeDLException) {
+            Log.e(TAG, "failed to initialize youtubedl-android", e)
+        }
     }
 
     companion object {
         lateinit var res: Resources
+        private const val TAG = "BaseApplication"
     }
 }
