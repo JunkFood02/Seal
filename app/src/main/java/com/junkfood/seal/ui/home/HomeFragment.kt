a26d096d761e7e4ecb105cd0532bee0b986a16d4
junkfood <junkfood@hust.edu.cn>
2022-04-19 13:49:56 +0800

    Code refactor, Toast error msgs

--- app/src/main/java/com/junkfood/seal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/junkfood/seal/ui/home/HomeFragment.kt
@@ -1,38 +1,30 @@
 package com.junkfood.seal.ui.home
 
 import android.Manifest
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.media.MediaScannerConnection
-import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
+import android.os.Message
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
-import androidx.core.content.PermissionChecker
-import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.junkfood.seal.BaseApplication
 import com.junkfood.seal.BaseApplication.Companion.downloadDir
 import com.junkfood.seal.databinding.FragmentHomeBinding
-import com.yausername.youtubedl_android.YoutubeDL
-import com.yausername.youtubedl_android.YoutubeDLRequest
-import com.yausername.youtubedl_android.mapper.VideoInfo
+import com.junkfood.seal.ui.home.HomeFragment.Companion.UPDATE_PROGRESS
+import com.junkfood.seal.util.DownloadUtil
 import java.io.File
 import java.util.*
 
@@ -44,6 +36,33 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var activityResultLauncher: ActivityResultLauncher<Array<String>>
+
+    val handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            when (msg.what) {
+                SHOW_ERROR_MESSAGE -> Toast.makeText(context, msg.obj as String, Toast.LENGTH_SHORT)
+                    .show()
+                UPDATE_PROGRESS -> {
+                    homeViewModel.updateProgress(msg.obj as Float)
+                }
+                FINISH_DOWNLOADING -> {
+                    startActivity(Intent().apply {
+                        action = (Intent.ACTION_VIEW)
+                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                        setDataAndType(
+                            FileProvider.getUriForFile(
+                                BaseApplication.context,
+                                BaseApplication.context.packageName + ".provider",
+                                File(msg.obj as String)
+                            ), if (msg.arg1 == 1) "audio/*" else "video/*"
+                        )
+                    })
+                }
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,13 +80,17 @@ class HomeFragment : Fragment() {
                     url = "https://youtu.be/t5c8D1xbXtw";
                 }
                 Toast.makeText(context, "Fetching video info.", Toast.LENGTH_SHORT).show()
-                getVideo(url)
+                DownloadUtil.getVideo(
+                    url, homeViewModel.audioSwitch.value!!,
+                    homeViewModel.thumbnailSwitch.value!!, handler
+                )
             } else {
                 Toast.makeText(context, "Failed to request permission", Toast.LENGTH_SHORT)
                     .show()
             }
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -138,97 +161,6 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun getVideo(url: String) {
-
-        Thread {
-            Looper.prepare()
-            val request = YoutubeDLRequest(url)
-            lateinit var ext: String
-            val videoInfo: VideoInfo = YoutubeDL.getInstance().getInfo(url)
-            var title: String = createFilename(videoInfo.title)
-            ext = videoInfo.ext
-
-            if (url.contains("list")) {
-                Toast.makeText(context, "Start downloading playlist.", Toast.LENGTH_SHORT).show()
-                request.addOption("-P", "$downloadDir/")
-                request.addOption("-o", "%(playlist)s/%(title)s.%(ext)s")
-                request.buildCommand()
-//              request.addOption("-o", "$downloadDir/%(title)s.%(ext)s")
-            } else {
-                Toast.makeText(context, "Start downloading '$title'", Toast.LENGTH_SHORT)
-                    .show()
-                request.addOption("-P", "$downloadDir/")
-                request.addOption("-o", "$title.%(ext)s")
-            }
-            if (homeViewModel.audioSwitch.value == true) {
-                request.addOption("-x")
-                request.addOption("--audio-format", "mp3")
-                request.addOption("--audio-quality", "0")
-                ext = "mp3"
-            }
-            if (homeViewModel.thumbnailSwitch.value == true) {
-                if (homeViewModel.audioSwitch.value == true) {
-                    request.addOption("--add-metadata")
-                    request.addOption("--embed-thumbnail")
-                    request.addOption("--compat-options", "embed-thumbnail-atomicparsley")
-                } else {
-                    request.addOption("--write-thumbnail")
-                    request.addOption("--convert-thumbnails", "jpg")
-                }
-            }
-            if (homeViewModel.proxy.value != "" && homeViewModel.proxySwitch.value == true) {
-                request.addOption("--proxy", homeViewModel.proxy.value!!)
-                Toast.makeText(
-                    context,
-                    "Downloading using proxy.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            request.addOption("--force-overwrites")
-            var noError = true
-            try {
-                YoutubeDL.getInstance().execute(
-                    request
-                ) { progress: Float, _: Long, s: String ->
-                    Log.d(TAG, s)
-                    homeViewModel.updateProgress(progress)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                noError = false
-                Toast.makeText(context, "Unknown error(s) occurred", Toast.LENGTH_SHORT).show()
-            }
-            if (noError) {
-                homeViewModel.updateProgress(100f)
-                Toast.makeText(context, "Download completed!", Toast.LENGTH_SHORT).show()
-                if (!url.contains("list")) {
-                    Log.d(TAG, "$downloadDir/$title.$ext")
-                    MediaScannerConnection.scanFile(
-                        context, arrayOf("$downloadDir/$title.$ext"),
-                        arrayOf(if (ext == "mp3") "audio/*" else "video/*"), null
-                    )
-                    startActivity(Intent().apply {
-                        action = (Intent.ACTION_VIEW)
-                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                        setDataAndType(
-                            FileProvider.getUriForFile(
-                                BaseApplication.context,
-                                BaseApplication.context.packageName + ".provider",
-                                File("$downloadDir/$title.$ext")
-                            ), if (ext == "mp3") "audio/*" else "video/*"
-                        )
-                    })
-                }
-            }
-        }.start()
-    }
-
-    private fun createFilename(title: String): String {
-        val cleanFileName = title.replace("[\\\\><\"|*?'%:#/]".toRegex(), "_")
-        var fileName = cleanFileName.trim { it <= ' ' }.replace(" +".toRegex(), " ")
-        if (fileName.length > 127) fileName = fileName.substring(0, 127)
-        return fileName + Date().time
-    }
 
     fun updateDownloadDir() {
         BaseApplication.updateDownloadDir()
@@ -236,6 +168,9 @@ class HomeFragment : Fragment() {
     }
 
     companion object {
+        const val UPDATE_PROGRESS = 1
+        const val FINISH_DOWNLOADING = 2
+        const val SHOW_ERROR_MESSAGE = -1
         private const val TAG = "HomeFragment"
     }
 }
---
+++ app/src/main/java/com/junkfood/seal/util/DownloadUtil.kt
@@ -0,0 +1,137 @@
+package com.junkfood.seal.util
+
+import android.media.MediaScannerConnection
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import android.widget.Toast
+import com.junkfood.seal.BaseApplication
+import com.junkfood.seal.BaseApplication.Companion.context
+import com.junkfood.seal.ui.home.HomeFragment
+import com.yausername.youtubedl_android.YoutubeDL
+import com.yausername.youtubedl_android.YoutubeDLRequest
+import com.yausername.youtubedl_android.mapper.VideoInfo
+
+object DownloadUtil {
+    private const val TAG = "DownloadUtil"
+    fun getVideo(url: String, extractAudio: Boolean, createThumbnail: Boolean, handler: Handler) {
+
+        Thread {
+            Looper.prepare()
+            val request = YoutubeDLRequest(url)
+            lateinit var ext: String
+            lateinit var title: String
+            var videoInfo: VideoInfo? = null
+            try {
+                videoInfo = YoutubeDL.getInstance().getInfo(url)
+                title = createFilename(videoInfo.title)
+                ext = videoInfo.ext
+            } catch (e: java.lang.Exception) {
+                handler.post {
+                    Toast.makeText(
+                        context,
+                        "Error occurred when fetching video info",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                Log.e(TAG, "getVideo: ", e)
+            }
+            if (videoInfo == null)
+                return@Thread
+            if (url.contains("list")) {
+                handler.post {
+                    Toast.makeText(
+                        context,
+                        "Start downloading playlist.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                request.addOption("-P", "${BaseApplication.downloadDir}/")
+                request.addOption("-o", "%(playlist)s/%(title)s.%(ext)s")
+                request.buildCommand()
+//              request.addOption("-o", "$downloadDir/%(title)s.%(ext)s")
+            } else {
+                handler.post {
+                    Toast.makeText(context, "Start downloading '$title'", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                request.addOption("-P", "${BaseApplication.downloadDir}/")
+                request.addOption("-o", "$title.%(ext)s")
+            }
+            if (extractAudio) {
+                request.addOption("-x")
+                request.addOption("--audio-format", "mp3")
+                request.addOption("--audio-quality", "0")
+                ext = "mp3"
+            }
+            if (createThumbnail) {
+                if (extractAudio) {
+                    request.addOption("--add-metadata")
+                    request.addOption("--embed-thumbnail")
+                    request.addOption("--compat-options", "embed-thumbnail-atomicparsley")
+                } else {
+                    request.addOption("--write-thumbnail")
+                    request.addOption("--convert-thumbnails", "jpg")
+                }
+            }
+/*            if (homeViewModel.proxy.value != "" && homeViewModel.proxySwitch.value == true) {
+                request.addOption("--proxy", homeViewModel.proxy.value!!)
+                Toast.makeText(
+                    context,
+                    "Downloading using proxy",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }*/
+            request.addOption("--force-overwrites")
+            try {
+                YoutubeDL.getInstance().execute(
+                    request
+                ) { progress: Float, _: Long, s: String ->
+                    Log.d(TAG, s)
+                    handler.handleMessage(Message().apply {
+                        what = HomeFragment.UPDATE_PROGRESS
+                        obj = progress
+                    })
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                handler.post {
+                    Toast.makeText(
+                        context,
+                        "Error occurred when downloading video",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return@Thread
+            }
+            handler.handleMessage(Message().apply {
+                what = HomeFragment.UPDATE_PROGRESS
+                obj = 100f
+            })
+            handler.post {
+                Toast.makeText(context, "Download completed!", Toast.LENGTH_SHORT).show()
+            }
+            if (!url.contains("list")) {
+                Log.d(TAG, "${BaseApplication.downloadDir}/$title.$ext")
+                MediaScannerConnection.scanFile(
+                    context, arrayOf("${BaseApplication.downloadDir}/$title.$ext"),
+                    arrayOf(if (ext == "mp3") "audio/*" else "video/*"), null
+                )
+                handler.handleMessage(Message().apply {
+                    what = HomeFragment.FINISH_DOWNLOADING
+                    obj = "${BaseApplication.downloadDir}/$title.$ext"
+                    arg1 = if (ext == "mp3") 1 else 0
+                })
+            }
+        }.start()
+    }
+
+    private fun createFilename(title: String): String {
+        val cleanFileName = title.replace("[\\\\><\"|*?'%:#/]".toRegex(), "_")
+        var fileName = cleanFileName.trim { it <= ' ' }.replace(" +".toRegex(), " ")
+        if (fileName.length > 127) fileName = fileName.substring(0, 127)
+        return fileName //+ Date().time
+    }
+
+}
