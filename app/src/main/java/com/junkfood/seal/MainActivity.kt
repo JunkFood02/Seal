b8a73298aec8ae0feb67a1f7593f615ecffedc77
junkfood <69683722+JunkFood02@users.noreply.github.com>
2022-04-20 03:18:42 +0800

    setImmersiveStatusBar

--- app/src/main/java/com/junkfood/seal/MainActivity.kt
+++ app/src/main/java/com/junkfood/seal/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
 import android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setImmersiveStatusBar()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navView: BottomNavigationView = binding.navView
@@ -38,6 +44,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setImmersiveStatusBar() {
+        if (Build.VERSION.SDK_INT >= 30) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v: View, windowInsets: WindowInsetsCompat ->
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(0, insets.top, 0, if (insets.bottom > 50) insets.bottom else 0)
+                WindowInsetsCompat.CONSUMED
+            }
+        } else window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
 
     companion object {
         private const val TAG = "MainActivity"
