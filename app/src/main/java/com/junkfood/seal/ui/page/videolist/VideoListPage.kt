e1d214259e4205322d8003538b53956d73229e62
junkfood <junkfood@hust.edu.cn>
2022-05-15 02:28:15 +0800

    FilterChip but Ugly codes

--- app/src/main/java/com/junkfood/seal/ui/page/videolist/VideoListPage.kt
+++ app/src/main/java/com/junkfood/seal/ui/page/videolist/VideoListPage.kt
@@ -1,17 +1,19 @@
 package com.junkfood.seal.ui.page.videolist
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.rememberSplineBasedDecay
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -19,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.junkfood.seal.R
+import com.junkfood.seal.ui.component.AudioListItem
 import com.junkfood.seal.ui.component.LargeTopAppBar
 import com.junkfood.seal.ui.component.VideoListItem
 import com.junkfood.seal.util.FileUtil
@@ -59,29 +62,112 @@ fun VideoListPage(
                     }
                 }, scrollBehavior = scrollBehavior, contentPadding = PaddingValues()
             )
-        },
-        content = { innerPadding ->
-            LazyColumn(
-                contentPadding = innerPadding,
-            ) {
-                for (item in list.value.reversed()) {
-                    item {
-                        VideoListItem(
-                            title = item.videoTitle,
-                            author = item.videoAuthor,
-                            thumbnailUrl = item.thumbnailUrl,
-                            videoUrl = item.videoUrl,
-                            isAudio = item.videoPath.contains(".mp3"),
-                            onClick = { FileUtil.openFile(item.videoPath) }
-                        ) {
-                            videoListViewModel.showDrawer(scope, item)
-                        }
+        }
+    ) { innerPadding ->
+        val audioFilter = remember { mutableStateOf(false) }
+        val videoFilter = remember { mutableStateOf(false) }
+        val ytbFilter = remember { mutableStateOf(false) }
+        val bilibiliFilter = remember { mutableStateOf(false) }
+        LazyColumn(
+            contentPadding = innerPadding,
+        ) {
+            item {
+                Row() {
+                    AnimatedVisibility(visible = !videoFilter.value) {
+                        FilterChipWithIcon(
+                            select = audioFilter.value,
+                            onClick = { audioFilter.value = !audioFilter.value },
+                            label = stringResource(id = R.string.audio)
+                        )
+                    }
+                    AnimatedVisibility(visible = !audioFilter.value) {
+                        FilterChipWithIcon(
+                            select = videoFilter.value,
+                            onClick = { videoFilter.value = !videoFilter.value },
+                            label = stringResource(id = R.string.video)
+                        )
+                    }
+                    AnimatedVisibility(visible = !ytbFilter.value) {
+                        FilterChipWithIcon(
+                            select = bilibiliFilter.value,
+                            onClick = { bilibiliFilter.value = !bilibiliFilter.value },
+                            label = stringResource(id = R.string.bilibili)
+                        )
+                    }
+                    AnimatedVisibility(visible = !bilibiliFilter.value) {
+                        FilterChipWithIcon(
+                            select = ytbFilter.value,
+                            onClick = { ytbFilter.value = !ytbFilter.value },
+                            label = "YouTube"
+                        )
                     }
                 }
-
             }
-        })
+            items(list.value.reversed()) {
+                AnimatedVisibility(
+                    visible = !audioFilter.value and (!ytbFilter.value or (ytbFilter.value and it.videoUrl.contains(
+                        "youtu"
+                    ))) and (!bilibiliFilter.value or it.videoUrl.contains(regex = Regex("(b23\\.tv)|(bilibili)")))
+                )
+                {
+                    with(it) {
+                        if (!videoPath.contains(".mp3"))
+                            VideoListItem(
+                                title = videoTitle,
+                                author = videoAuthor,
+                                thumbnailUrl = thumbnailUrl,
+                                videoUrl = videoUrl,
+                                onClick = { FileUtil.openFile(videoPath) }
+                            ) { videoListViewModel.showDrawer(scope, this@with) }
+                    }
+                }
+            }
+            items(list.value.reversed()) {
+                AnimatedVisibility(
+                    visible = !videoFilter.value and (!ytbFilter.value or (ytbFilter.value and it.videoUrl.contains(
+                        "youtu"
+                    ))) and (!bilibiliFilter.value or it.videoUrl.contains(regex = Regex("(b23\\.tv)|(bilibili)")))
+                ) {
+                    with(it) {
+                        if (videoPath.contains(".mp3"))
+                            AudioListItem(
+                                title = videoTitle,
+                                author = videoAuthor,
+                                thumbnailUrl = thumbnailUrl,
+                                videoUrl = videoUrl,
+                                onClick = { FileUtil.openFile(videoPath) }
+                            ) { videoListViewModel.showDrawer(scope, this@with) }
+                    }
+                }
+            }
+        }
+
+    }
     VideoDetailDrawer()
     if (viewState.value.showDialog)
         RemoveItemDialog()
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterChipWithIcon(select: Boolean, onClick: () -> Unit, label: String) {
+    FilterChip(
+        modifier = Modifier.padding(start = 12.dp),
+        selected = select,
+        onClick = onClick,
+        label = {
+            Text(text = label)
+        },
+        trailingIcon = {
+            AnimatedVisibility(visible = select) {
+                Icon(
+                    Icons.Outlined.Check,
+                    stringResource(R.string.checked),
+                    modifier = Modifier.size(18.dp)
+                )
+            }
+        },
+    )
+}
+
+
