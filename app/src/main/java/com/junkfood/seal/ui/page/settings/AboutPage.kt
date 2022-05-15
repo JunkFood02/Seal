609a39a06e27fc3f24446742d375055c5d89d4df
junkfood <junkfood@hust.edu.cn>
2022-05-15 15:52:26 +0800

    Credits

--- app/src/main/java/com/junkfood/seal/ui/page/settings/AboutPage.kt
+++ app/src/main/java/com/junkfood/seal/ui/page/settings/AboutPage.kt
@@ -3,6 +3,7 @@ package com.junkfood.seal.ui.page.settings
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.animation.rememberSplineBasedDecay
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +11,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -34,7 +36,20 @@ fun AboutPage(navController: NavController) {
     val versionName = info.versionName
     val releaseURL = "https://github.com/JunkFood02/Seal/releases/latest"
     val repoUrl = "https://github.com/JunkFood02/Seal"
-
+    val youtubedlAndroidUrl = "https://github.com/yausername/youtubedl-android"
+    val ytdlpUrl = "https://github.com/yt-dlp/yt-dlp"
+    val readYou = "https://github.com/Ashinch/ReadYou"
+    val muscYou = "https://github.com/Kyant0/MusicYou"
+    val dvd = "https://github.com/yausername/dvd"
+    val icons8 = "https://icons8.com/"
+    val materialIcon = "https://fonts.google.com/icons"
+    val creditsDialog = remember { mutableStateOf(false) }
+    fun openUrl(url: String) {
+        context.startActivity(Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(url)
+        })
+    }
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
@@ -65,12 +80,7 @@ fun AboutPage(navController: NavController) {
                         description = stringResource(R.string.readme_desc),
                         icon = null,
                         enable = true
-                    ) {
-                        context.startActivity(Intent().apply {
-                            action = Intent.ACTION_VIEW
-                            data = Uri.parse(repoUrl)
-                        })
-                    }
+                    ) { openUrl(repoUrl) }
                 }
                 item {
                     PreferenceItem(
@@ -86,14 +96,37 @@ fun AboutPage(navController: NavController) {
                         description = stringResource(R.string.release_desc),
                         icon = null,
                         enable = true
+                    ) { openUrl(releaseURL) }
+                }
+                item {
+                    PreferenceItem(
+                        title = stringResource(id = R.string.credits),
+                        description = stringResource(id = R.string.credits_desc),
+                        icon = null,
+                        enable = true
                     ) {
-                        context.startActivity(Intent().apply {
-                            action = Intent.ACTION_VIEW
-                            data = Uri.parse(releaseURL)
-                        })
+                        creditsDialog.value = true
                     }
                 }
             }
         })
-
+    if (creditsDialog.value)
+        AlertDialog(
+            onDismissRequest = { creditsDialog.value = false },
+            title = { Text(stringResource(id = R.string.credits)) },
+            text = {
+                Column() {
+                    TextButton(onClick = { openUrl(youtubedlAndroidUrl) }) { Text(text = "youtubedl-android") }
+                    TextButton(onClick = { openUrl(ytdlpUrl) }) { Text(text = "yt-dlp") }
+                    TextButton(onClick = { openUrl(readYou) }) { Text(text = "Read You") }
+                    TextButton(onClick = { openUrl(muscYou) }) { Text(text = "Music You") }
+                    TextButton(onClick = { openUrl(dvd) }) { Text(text = "dvd") }
+                    TextButton(onClick = { openUrl(materialIcon) }) { Text("Material Icons") }
+                    TextButton(onClick = { openUrl(icons8) }) { Text(text = "App Icon by Icons8.com") }
+                }
+            }, confirmButton = {
+                TextButton(onClick = { creditsDialog.value = false }) {
+                    Text(text = stringResource(id = R.string.confirm))
+                }
+            })
 }
