b82075b30684b66cf95d16c2efeaf82072a69f4e
junkfood <69683722+JunkFood02@users.noreply.github.com>
2022-09-19 16:33:00 +0800

    Create changelogs for v1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ apply(plugin = "dagger.hilt.android.plugin")
 
 val versionMajor = 1
 val versionMinor = 3
-val versionPatch = 1
+val versionPatch = 2
 val versionBuild = 0
 val isStable = true
 
@@ -52,7 +52,7 @@ android {
         applicationId = "com.junkfood.seal"
         minSdk = 23
         targetSdk = 33
-        versionCode = 10310
+        versionCode = 10320
         versionName = StringBuilder("${versionMajor}.${versionMinor}.${versionPatch}").apply {
             if (!isStable) append("-beta.${versionBuild}")
             if (!splitApks) append("-(F-Droid)")
