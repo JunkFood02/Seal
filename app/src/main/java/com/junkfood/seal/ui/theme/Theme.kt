7c370767aa33b7bdc026a8a08bec33207d3bbd6e
Gabriel Fontán <gabilessto@gmail.com>
2022-10-22 14:16:13 +0200

    Deleted commented SealTheme

--- app/src/main/java/com/junkfood/seal/ui/theme/Theme.kt
+++ app/src/main/java/com/junkfood/seal/ui/theme/Theme.kt
@@ -12,11 +12,9 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.color.MaterialColors
 import com.junkfood.seal.ui.theme.ColorScheme.colorSchemeFromColor
-
 
 fun Color.applyOpacity(enabled: Boolean): Color {
     return if (enabled) this else this.copy(alpha = 0.62f)
@@ -75,40 +73,3 @@ fun SealTheme(
         content = content
     )
 }
-
-/*@Composable
-fun SealTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    seedColor: Int,
-    isDynamicColorEnabled: Boolean,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        DynamicColors.isDynamicColorAvailable() && isDynamicColorEnabled -> {
-            val context = LocalContext.current
-            if (darkTheme) {
-                dynamicDarkColorScheme(context)
-            } else {
-                dynamicLightColorScheme(context)
-            }
-        }
-        else -> colorSchemeFromColor(seedColor, darkTheme)
-    }
-    val view = LocalView.current
-    if(!view.isInEditMode) {
-        val currentWindow = (view.context as? Activity)?.window
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = android.graphics.Color.TRANSPARENT
-            (view.context as Activity).window.navigationBarColor = android.graphics.Color.TRANSPARENT
-            WindowCompat.getInsetsController(currentWindow!!, view).isAppearanceLightStatusBars =
-                darkTheme
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
-}*/
