380ea3597ea291da8a41484028e5605242a7649d
junkfood <69683722+junkfood02@users.noreply.github.com>
2022-10-23 02:11:12 +0800

    Bug Fix: Blank screen on MIUI devices

--- app/src/main/java/com/junkfood/seal/ui/theme/Theme.kt
+++ app/src/main/java/com/junkfood/seal/ui/theme/Theme.kt
@@ -1,17 +1,20 @@
 package com.junkfood.seal.ui.theme
 
 import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.view.Window
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.color.MaterialColors
 import com.junkfood.seal.ui.theme.ColorScheme.colorSchemeFromColor
@@ -30,6 +33,13 @@ fun Color.harmonizeWithPrimary(): Color {
     )
 }
 
+private tailrec fun Context.findWindow(): Window? =
+    when (this) {
+        is Activity -> window
+        is ContextWrapper -> baseContext.findWindow()
+        else -> null
+    }
+
 @Composable
 fun SealTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -47,6 +57,7 @@ fun SealTheme(
                 dynamicLightColorScheme(context)
             }
         }
+
         else -> colorSchemeFromColor(seedColor, darkTheme)
     }.run {
         if (isHighContrastModeEnabled && darkTheme) copy(
@@ -55,16 +66,12 @@ fun SealTheme(
         )
         else this
     }
+    val window = LocalView.current.context.findWindow()
     val view = LocalView.current
-    if(!view.isInEditMode){
-        val currentWindow = (view.context as? Activity)?.window
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = android.graphics.Color.TRANSPARENT
-            (view.context as Activity).window.navigationBarColor = android.graphics.Color.TRANSPARENT
-            WindowCompat.getInsetsController(currentWindow!!, view).isAppearanceLightStatusBars =
-                darkTheme
-        }
-    }
+
+    window?.let { WindowCompat.getInsetsController(it, view).isAppearanceLightStatusBars = darkTheme }
+
+    rememberSystemUiController(window).setSystemBarsColor(Color.Transparent, !darkTheme)
 
     MaterialTheme(
         colorScheme = colorScheme,
