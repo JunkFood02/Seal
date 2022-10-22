e5163db26a3c99e5809bde7cad1690bb413f3e8b
Gabriel Fontán <gabilessto@gmail.com>
2022-10-22 14:14:44 +0200

    Fixed MIUI devices bug showing the app in blank (finally)

--- app/src/main/java/com/junkfood/seal/ui/theme/Theme.kt
+++ app/src/main/java/com/junkfood/seal/ui/theme/Theme.kt
@@ -1,13 +1,17 @@
 package com.junkfood.seal.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.color.MaterialColors
@@ -36,12 +40,6 @@ fun SealTheme(
     isDynamicColorEnabled: Boolean,
     content: @Composable () -> Unit
 ) {
-    rememberSystemUiController().run {
-        setStatusBarColor(Color.Transparent, !darkTheme)
-        setSystemBarsColor(Color.Transparent, !darkTheme)
-        setNavigationBarColor(Color.Transparent, !darkTheme)
-    }
-
     val colorScheme = when {
         DynamicColors.isDynamicColorAvailable() && isDynamicColorEnabled -> {
             val context = LocalContext.current
@@ -51,7 +49,6 @@ fun SealTheme(
                 dynamicLightColorScheme(context)
             }
         }
-
         else -> colorSchemeFromColor(seedColor, darkTheme)
     }.run {
         if (isHighContrastModeEnabled && darkTheme) copy(
@@ -59,6 +56,16 @@ fun SealTheme(
             background = Color.Black,
         )
         else this
+    }
+    val view = LocalView.current
+    if(!view.isInEditMode){
+        val currentWindow = (view.context as? Activity)?.window
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = android.graphics.Color.TRANSPARENT
+            (view.context as Activity).window.navigationBarColor = android.graphics.Color.TRANSPARENT
+            WindowCompat.getInsetsController(currentWindow!!, view).isAppearanceLightStatusBars =
+                darkTheme
+        }
     }
 
     MaterialTheme(
@@ -68,3 +75,40 @@ fun SealTheme(
         content = content
     )
 }
+
+/*@Composable
+fun SealTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    seedColor: Int,
+    isDynamicColorEnabled: Boolean,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        DynamicColors.isDynamicColorAvailable() && isDynamicColorEnabled -> {
+            val context = LocalContext.current
+            if (darkTheme) {
+                dynamicDarkColorScheme(context)
+            } else {
+                dynamicLightColorScheme(context)
+            }
+        }
+        else -> colorSchemeFromColor(seedColor, darkTheme)
+    }
+    val view = LocalView.current
+    if(!view.isInEditMode) {
+        val currentWindow = (view.context as? Activity)?.window
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = android.graphics.Color.TRANSPARENT
+            (view.context as Activity).window.navigationBarColor = android.graphics.Color.TRANSPARENT
+            WindowCompat.getInsetsController(currentWindow!!, view).isAppearanceLightStatusBars =
+                darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}*/
