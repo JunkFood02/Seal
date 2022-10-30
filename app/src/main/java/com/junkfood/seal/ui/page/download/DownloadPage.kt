a80dabd6f9e7f40ddd19c72304e08571ea1c8d1a
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2022-10-30 22:32:16 +1100

    Add bottom spacer

--- app/src/main/java/com/junkfood/seal/ui/page/download/DownloadPage.kt
+++ app/src/main/java/com/junkfood/seal/ui/page/download/DownloadPage.kt
@@ -16,15 +16,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
@@ -239,6 +231,7 @@ fun DownloadPage(
                         )
                     }
                     NavigationBarSpacer()
+                    FABSpacer()
                 }
             }
             DownloadSettingDialog(
@@ -430,4 +423,9 @@ fun FABs(
         )
     }
 
+}
+
+@Composable
+fun FABSpacer(modifier: Modifier = Modifier) {
+    Spacer(modifier = modifier.height((2*56+2*12+8+6).dp))
 }
