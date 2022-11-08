9ddf59e66f8ef5639eef167493a7856aa6b41d9a
junkfood <69683722+junkfood02@users.noreply.github.com>
2022-11-08 12:39:20 +0800

    Fix CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
             arguments {
                 arg("room.schemaLocation", "$projectDir/schemas")
             }
+            correctErrorTypes = true
         }
         if (!splitApks)
             ndk {
@@ -133,8 +134,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
 
     implementation(libs.androidx.compose.ui)
-//    implementation(libs.androidx.compose.ui.tooling)
-//    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material.iconsExtended)
     implementation(libs.androidx.compose.material3)
