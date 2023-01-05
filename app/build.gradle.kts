995e2af9490e20854deeced8ff24179c1a15fa38
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-01-05 20:42:50 +0800

    Release v1.7.2 (10720)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ sealed class Version(
 val currentVersion: Version = Version.Stable(
     versionMajor = 1,
     versionMinor = 7,
-    versionPatch = 1,
+    versionPatch = 2,
 )
 
 val keystorePropertiesFile = rootProject.file("keystore.properties")
@@ -71,7 +71,7 @@ android {
         applicationId = "com.junkfood.seal"
         minSdk = 23
         targetSdk = 33
-        versionCode = 10710
+        versionCode = 10720
         versionName = currentVersion.toVersionName().run {
             if (!splitApks) "$this-(F-Droid)"
             else this
