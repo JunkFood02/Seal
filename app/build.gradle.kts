d93d72ff7cc61ef6d40763f990140d0b5e7398ee
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-02-24 01:30:36 +0800

    Add SVG & Dynamic color implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -207,7 +207,7 @@ dependencies {
     implementation(libs.youtubedl.android.library)
     implementation(libs.youtubedl.android.ffmpeg)
     implementation(libs.youtubedl.android.aria2c)
-
+    implementation(libs.caverock.androidsvg)
 //    implementation("com.github.xibr.youtubedl-android:library:$youtubedlAndroidVersion")
 //    implementation("com.github.xibr.youtubedl-android:ffmpeg:$youtubedlAndroidVersion")
 //    implementation("com.github.xibr.youtubedl-android:aria2c:$youtubedlAndroidVersion")
