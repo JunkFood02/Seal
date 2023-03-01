c8927c48e8bfba0bf59e6f2e4298668c4255135e
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-03-02 03:32:19 +0800

    Update strings

--- app/src/main/java/com/junkfood/seal/ui/page/settings/about/DonatePage.kt
+++ app/src/main/java/com/junkfood/seal/ui/page/settings/about/DonatePage.kt
@@ -39,7 +39,7 @@ fun DonatePage(onBackPressed: () -> Unit) {
                 title = {
                     Text(
                         modifier = Modifier,
-                        text = stringResource(id = R.string.donate),
+                        text = stringResource(id = R.string.sponsor),
                     )
                 }, navigationIcon = {
                     BackButton {
