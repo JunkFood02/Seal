e0e446467cf1337a630cddcd2e90cde6d868f88b
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-05-07 03:16:38 +0800

    Update `ColorScheme` to reflect the new `Fixed` roles

--- app/src/main/java/com/junkfood/seal/ui/theme/ColorScheme.kt
+++ app/src/main/java/com/junkfood/seal/ui/theme/ColorScheme.kt
@@ -3,6 +3,9 @@ package com.junkfood.seal.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.junkfood.seal.ui.common.LocalDarkTheme
+import com.kyant.monet.a1
+import com.kyant.monet.a2
+import com.kyant.monet.a3
 import com.kyant.monet.n1
 
 @Composable
@@ -26,6 +29,33 @@ fun Number.autoDark(isDarkTheme: Boolean = LocalDarkTheme.current.isDarkTheme())
         100.0 -> 20.0
         else -> this.toDouble()
     }
+
+object FixedAccentColors {
+    val primaryFixed: Color
+        @Composable get() = 90.a1
+    val primaryFixedDim: Color
+        @Composable get() = 80.a1
+    val onPrimaryFixed: Color
+        @Composable get() = 10.a1
+    val onPrimaryFixedVariant: Color
+        @Composable get() = 30.a1
+    val secondaryFixed: Color
+        @Composable get() = 90.a2
+    val secondaryFixedDim: Color
+        @Composable get() = 80.a2
+    val onSecondaryFixed: Color
+        @Composable get() = 10.a2
+    val onSecondaryFixedVariant: Color
+        @Composable get() = 30.a2
+    val tertiaryFixed: Color
+        @Composable get() = 90.a3
+    val tertiaryFixedDim: Color
+        @Composable get() = 80.a3
+    val onTertiaryFixed: Color
+        @Composable get() = 10.a3
+    val onTertiaryFixedVariant: Color
+        @Composable get() = 30.a3
+}
 
 object Surfaces {
     const val LOWEST = 0
