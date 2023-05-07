d64e492b2fba931f2c0226c4a99c31b1d11b1382
Gabriel Fontán <gabilessto@gmail.com>
2023-05-07 21:14:38 +0200

    changed color module namespace )idk why i hav added a 02 xd)

--- color/build.gradle.kts
+++ color/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 21
         targetSdk = 33
     }
-    namespace = "com.junkfood02.seal.color"
+    namespace = "com.junkfood.seal.color"
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
     }
