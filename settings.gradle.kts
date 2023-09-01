bba0a60b3830d29b6f22211a3ec0fe1c07a36c7b
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-09-01 18:29:41 +0800

    fix: add toolchain resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,9 @@ pluginManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
