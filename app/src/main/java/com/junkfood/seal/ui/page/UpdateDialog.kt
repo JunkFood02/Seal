f90ae02d737f892dc1a22bd2e224f67da4944d0e
junkfood <69683722+JunkFood02@users.noreply.github.com>
2024-04-05 04:07:03 +0800

    feat(ui): internal changes

--- app/src/main/java/com/junkfood/seal/ui/page/UpdateDialog.kt
+++ app/src/main/java/com/junkfood/seal/ui/page/UpdateDialog.kt
@@ -1,16 +1,19 @@
 package com.junkfood.seal.ui.page
 
 import android.os.Build
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.NewReleases
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,11 +22,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.junkfood.seal.R
-import com.junkfood.seal.ui.component.DismissButton
 import com.junkfood.seal.util.ToastUtil
 import com.junkfood.seal.util.UpdateUtil
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 
@@ -76,17 +83,54 @@ fun UpdateDialogImpl(
         onDismissRequest = {},
         title = { Text(title) },
         icon = { Icon(Icons.Outlined.NewReleases, null) }, confirmButton = {
-            TextButton(onClick = { if (downloadStatus !is UpdateUtil.DownloadStatus.Progress) onConfirmUpdate() }) {
-                when (downloadStatus) {
-                    is UpdateUtil.DownloadStatus.Progress -> Text("${downloadStatus.percent} %")
-                    else -> Text(stringResource(R.string.update))
-                }
+
+            Button(
+                onClick = { if (downloadStatus !is UpdateUtil.DownloadStatus.Progress) onConfirmUpdate() }) {
+                Text(
+                    when (downloadStatus) {
+                        is UpdateUtil.DownloadStatus.Progress -> "${downloadStatus.percent} %"
+                        else -> stringResource(R.string.update)
+                    },
+                    modifier = Modifier.animateContentSize(),
+                    )
             }
         }, dismissButton = {
-            DismissButton { onDismissRequest() }
+            OutlinedButton(onClick = onDismissRequest) { Text(text = stringResource(id = R.string.dismiss)) }
         }, text = {
             Column(Modifier.verticalScroll(rememberScrollState())) {
                 Text(releaseNote)
             }
         })
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    var b by remember {
+        mutableStateOf(false)
+    }
+    val flow: MutableStateFlow<UpdateUtil.DownloadStatus> = remember {
+        MutableStateFlow(UpdateUtil.DownloadStatus.NotYet)
+    }
+
+    LaunchedEffect(b) {
+        if (b) {
+            repeat(100) { i ->
+                flow.update { UpdateUtil.DownloadStatus.Progress(percent = i) }
+                delay(50)
+            }
+        } else {
+            flow.update { UpdateUtil.DownloadStatus.NotYet }
+        }
+    }
+
+    val status by flow.collectAsStateWithLifecycle()
+
+    UpdateDialogImpl(
+        onDismissRequest = { b = false },
+        title = "v1.12.0",
+        onConfirmUpdate = { b = true },
+        releaseNote = "ReleaseNoteHTML",
+        downloadStatus = status
+    )
 }
