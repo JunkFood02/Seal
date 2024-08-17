e62553b267a424357981d8810710a0fef75a37e1
junkfood <69683722+JunkFood02@users.noreply.github.com>
2024-08-17 17:57:25 +0800

    feat(download): download task & state management (WIP)

--- app/src/main/java/com/junkfood/seal/download/DownloaderV2.kt
+++ app/src/main/java/com/junkfood/seal/download/DownloaderV2.kt
@@ -1,13 +1,119 @@
 package com.junkfood.seal.download
 
+import androidx.compose.runtime.mutableStateMapOf
+import com.junkfood.seal.download.DownloaderV2.state
+import com.junkfood.seal.download.Task.RestartableAction.Download
+import com.junkfood.seal.download.Task.RestartableAction.FetchInfo
+import com.junkfood.seal.download.Task.State
+import com.junkfood.seal.download.Task.State.Completed
+import com.junkfood.seal.download.Task.State.Error
+import com.junkfood.seal.download.Task.State.FetchingInfo
+import com.junkfood.seal.download.Task.State.Idle
+import com.junkfood.seal.download.Task.State.ReadyWithInfo
+import com.junkfood.seal.download.Task.State.Running
+import com.junkfood.seal.util.DownloadUtil
+import com.yausername.youtubedl_android.YoutubeDL
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 object DownloaderV2 {
     private val scope = CoroutineScope(SupervisorJob())
 
+    init {
+        scope.launch(Dispatchers.Default) { runningTaskFlow.collect {} }
+    }
+
     private val mRunningTaskFlow = MutableStateFlow(0)
     val runningTaskFlow = mRunningTaskFlow.asStateFlow()
+
+    val taskStateMap = mutableStateMapOf<Task, State>()
+
+    var Task.state: State
+        get() = taskStateMap[this]!!
+        set(value) {
+            taskStateMap[this] = value
+        }
+
+    fun Task.fetchInfo() {
+        val task = this
+        check(state == Idle)
+        scope
+            .launch {
+                DownloadUtil.fetchVideoInfoFromUrl(
+                        url = url, preferences = preferences, taskKey = id)
+                    .onSuccess { info ->
+                        taskStateMap.remove(task)
+                        taskStateMap += task.copy(info = info) to ReadyWithInfo
+                    }
+                    .onFailure { throwable ->
+                        task.state = Error(throwable = throwable, action = FetchInfo)
+                    }
+            }
+            .also { job ->
+                job.invokeOnCompletion { mRunningTaskFlow.update { i -> i - 1 } }
+                state = FetchingInfo(job = job, taskId = id)
+            }
+        mRunningTaskFlow.update { i -> i + 1 }
+    }
+
+    fun Task.download() {
+        check(state == ReadyWithInfo && info != null)
+        scope
+            .launch {
+                DownloadUtil.downloadVideo(
+                        videoInfo = info,
+                        taskId = id,
+                        downloadPreferences = preferences,
+                        progressCallback = { progress, _, text ->
+                            when (val preState = state) {
+                                is Running ->
+                                    state = preState.copy(progress = progress, progressText = text)
+                                else -> {}
+                            }
+                        })
+                    .onSuccess { pathList -> state = Completed(pathList.firstOrNull()) }
+                    .onFailure { throwable ->
+                        state = Error(throwable = throwable, action = Download)
+                    }
+            }
+            .also { job ->
+                job.invokeOnCompletion { mRunningTaskFlow.update { i -> i - 1 } }
+                state = Running(job = job, taskId = id)
+            }
+        mRunningTaskFlow.update { i -> i + 1 }
+    }
+
+    fun Task.cancel() {
+        check(state is State.Cancelable)
+        when (val preState = state) {
+            is State.Cancelable -> {
+                val res = YoutubeDL.destroyProcessById(preState.taskId)
+                if (res) {
+                    preState.job.cancel()
+                    state = State.Canceled(action = preState.action)
+                    mRunningTaskFlow.update { i -> i - 1 }
+                }
+            }
+            else -> {}
+        }
+    }
+
+    fun Task.restart() {
+        check(state is State.Restartable)
+        when (val preState = state) {
+            is State.Restartable -> {
+                state =
+                    when (preState.action) {
+                        Download -> ReadyWithInfo
+                        FetchInfo -> Idle
+                    }
+            }
+            else -> {}
+        }
+    }
 }
--- app/src/main/java/com/junkfood/seal/download/Task.kt
+++ app/src/main/java/com/junkfood/seal/download/Task.kt
@@ -6,24 +6,78 @@ import kotlinx.coroutines.Job
 
 data class Task(
     val url: String,
-    val state: State = State.Idle,
     val info: VideoInfo? = null,
-    private val preferences: DownloadUtil.DownloadPreferences,
+    val preferences: DownloadUtil.DownloadPreferences,
+    val id: String
 ) {
     sealed interface State {
 
+        interface Cancelable {
+            val job: Job
+            val taskId: String
+            val action: RestartableAction
+        }
+
+        interface Restartable {
+            val action: RestartableAction
+        }
+
         data object Idle : State
 
-        data class FetchingInfo(val job: Job, val taskId: String) : State
+        data class FetchingInfo(
+            override val job: Job,
+            override val taskId: String,
+        ) : State, Cancelable {
+            override val action: RestartableAction = RestartableAction.FetchInfo
+        }
 
         data object ReadyWithInfo : State
 
-        data class Running(val job: Job, val taskId: String, val progress: Float) : State
+        data class Running(
+            override val job: Job,
+            override val taskId: String,
+            val progress: Float = PROGRESS_INDETERMINATE,
+            val progressText: String = ""
+        ) : State, Cancelable {
+            override val action: RestartableAction = RestartableAction.Download
+        }
 
-        data object Canceled : State
+        data class Canceled(override val action: RestartableAction) : State, Restartable
 
-        data class Error(val throwable: Throwable) : State
+        data class Error(val throwable: Throwable, override val action: RestartableAction) :
+            State, Restartable
 
-        data class Completed(val filePath: String) : State
+        data class Completed(val filePath: String?) : State
+    }
+
+    sealed interface RestartableAction {
+        data object FetchInfo : RestartableAction
+
+        data object Download : RestartableAction
+    }
+
+    companion object {
+        const val PROGRESS_INDETERMINATE = -1f
+
+        fun createWithURL(
+            url: String,
+            preferences: DownloadUtil.DownloadPreferences =
+                DownloadUtil.DownloadPreferences.createFromPreferences()
+        ): Task {
+            return Task(url = url, preferences = preferences, id = makeId(url, preferences))
+        }
+
+        fun createWithInfo(
+            info: VideoInfo,
+            url: String = info.originalUrl.toString(),
+            preferences: DownloadUtil.DownloadPreferences =
+                DownloadUtil.DownloadPreferences.createFromPreferences()
+        ): Task {
+            return Task(
+                url = url, preferences = preferences, info = info, id = makeId(url, preferences))
+        }
+
+        private fun makeId(url: String, preferences: DownloadUtil.DownloadPreferences): String =
+            "${url}_${preferences.hashCode()}"
     }
 }
