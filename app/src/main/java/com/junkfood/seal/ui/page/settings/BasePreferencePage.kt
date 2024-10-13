8ffb1f9d01ad8282c98f37a6a8c912e29e24d000
junkfood <69683722+JunkFood02@users.noreply.github.com>
2024-10-14 03:41:32 +0800

    feat(ui): add troubleshooting page (wip)

---
+++ app/src/main/java/com/junkfood/seal/ui/page/settings/BasePreferencePage.kt
@@ -0,0 +1,57 @@
+package com.junkfood.seal.ui.page.settings
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.junkfood.seal.ui.component.BackButton
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BasePreferencePage(
+    modifier: Modifier = Modifier,
+    title: String,
+    onBack: () -> Unit,
+    topBar: @Composable (() -> Unit)? = null,
+    bottomBar: @Composable (() -> Unit) = {},
+    snackbarHost: @Composable () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    containerColor: Color = MaterialTheme.colorScheme.background,
+    contentColor: Color = contentColorFor(containerColor),
+    contentWindowInsets: WindowInsets = ScaffoldDefaults.contentWindowInsets,
+    content: @Composable (paddingValues: PaddingValues) -> Unit,
+) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val topBar =
+        topBar
+            ?: {
+                LargeTopAppBar(
+                    title = { Text(text = title) },
+                    scrollBehavior = scrollBehavior,
+                    navigationIcon = { BackButton(onClick = onBack) },
+                )
+            }
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackbarHost,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        contentWindowInsets = contentWindowInsets,
+        content = content,
+    )
+}
---
+++ app/src/main/java/com/junkfood/seal/ui/page/settings/troubleshooting/TroubleshootingPage.kt
@@ -0,0 +1,13 @@
+package com.junkfood.seal.ui.page.settings.troubleshooting
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TroubleShootingPage(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier.fillMaxSize()) { Column(modifier = Modifier.padding(it)) {} }
+}
